36dbe9996b705bf0ecc415928f20ae8896ad3e52
Banimir Putnik <bputnik@gmail.com>
2020-08-09 19:40:28 +0200

    Dodata tabela trustees i foreignID u tabelu students

--- database/migrations/2020_08_09_164257_create_trustees_table.php
+++ database/migrations/2020_08_09_164257_create_trustees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class CreateTrusteesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('trustees', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('trustee_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-            $table->string('password');
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
-            $table->date('dob')->nullable();
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('trustees');
     }
 }
