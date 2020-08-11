33d147e6e8fec7498f88cbced54f6dd5c0d5b52c
Banimir Putnik <bputnik@gmail.com>
2020-08-11 09:48:40 +0200

    Dodata forma za unos jezika

--- resources/views/admin/languages/create.blade.php
+++ resources/views/admin/languages/create.blade.php
@@ -4,6 +4,25 @@
 
         <h1>Dodavanje jezika</h1>
 
+        <div class="col-sm-3 form-group">
+        <form action="" method="post" enctype="text/plain">
+            @csrf
+
+            <label for="name">Naziv jezika</label>
+            <input type="text"
+                   name="name"
+                   id="name"
+                    class="form-control"
+                    placeholder="npr. francuski">
+
+        </form>
+
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Snimi</button>
+        </div>
+
 
     @endsection
 
