11c19a2d62f4a26c7479233b777d607a95506ad4
Banimir Putnik <bputnik@gmail.com>
2020-08-16 16:51:37 +0200

    Dodata welcome strana

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,270 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <title>Veb sistem</title>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('vendor-landing/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Custom fonts for this template -->
+    <link href="{{asset('vendor-landing/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor-landing/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/landing-page.css')}}" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+</head>
 
-            .content {
-                text-align: center;
-            }
+<body>
 
-            .title {
-                font-size: 84px;
-            }
+<!-- Navigation -->
+{{--<nav class="navbar navbar-light bg-light static-top">--}}
+{{--    <div class="container">--}}
+{{--        <a class="navbar-brand" href="#">Start Bootstrap</a>--}}
+{{--        <a class="btn btn-primary" href="#">Sign In</a>--}}
+{{--    </div>--}}
+{{--</nav>--}}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+<!-- Masthead -->
+{{--<header class="masthead text-white text-center">--}}
+{{--    <div class="overlay"></div>--}}
+{{--    <div class="container">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-xl-9 mx-auto">--}}
+{{--                <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">--}}
+{{--                <form>--}}
+{{--                    <div class="form-row">--}}
+{{--                        <div class="col-12 col-md-9 mb-2 mb-md-0">--}}
+{{--                            <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">--}}
+{{--                        </div>--}}
+{{--                        <div class="col-12 col-md-3">--}}
+{{--                            <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</header>--}}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<div class="d-flex"></div>
+<div class="title text-center mt-lg-5">
+    <h1>Veb sistem za poslovanje škole stranih jezika</h1>
+</div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+
+
+<!-- Icons Grid -->
+<section class="features-icons bg-light text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                    <a href="/login/admin" style="text-decoration: none">
+                    <div class="features-icons-icon d-flex">
+                        <i class="icon-settings m-auto text-primary"></i>
+                    </div>
+                    </a>
+                    <h3>Administratori</h3>
+{{--                    <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>--}}
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+            <div class="col-lg-4">
+                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                    <a href="/login/teacher" style="text-decoration: none">
+                    <div class="features-icons-icon d-flex">
+                        <i class="icon-note m-auto text-primary"></i>
+                    </div>
+                    </a>
+                    <h3>Nastavnici</h3>
+{{--                    <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>--}}
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                    <div class="features-icons-icon d-flex">
+                        <i class="icon-graduation m-auto text-primary"></i>
+                    </div>
+                    <h3>Učenici</h3>
+{{--                    <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>--}}
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+
+<!-- Image Showcases -->
+
+
+
+
+<!-- Footer -->
+<footer class="footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Contact</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Terms of Use</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                </ul>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-facebook fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item mr-3">
+                        <a href="#">
+                            <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <i class="fab fa-instagram fa-2x fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('vendor-landing/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor-landing/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+</body>
+
 </html>
+
+
+
+
+{{--<!DOCTYPE html>--}}
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+{{--    <head>--}}
+{{--        <meta charset="utf-8">--}}
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+
+{{--        <title>Laravel</title>--}}
+
+{{--        <!-- Fonts -->--}}
+{{--        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">--}}
+
+{{--        <!-- Styles -->--}}
+{{--        <style>--}}
+{{--            html, body {--}}
+{{--                background-color: #fff;--}}
+{{--                color: #636b6f;--}}
+{{--                font-family: 'Nunito', sans-serif;--}}
+{{--                font-weight: 200;--}}
+{{--                height: 100vh;--}}
+{{--                margin: 0;--}}
+{{--            }--}}
+
+{{--            .full-height {--}}
+{{--                height: 100vh;--}}
+{{--            }--}}
+
+{{--            .flex-center {--}}
+{{--                align-items: center;--}}
+{{--                display: flex;--}}
+{{--                justify-content: center;--}}
+{{--            }--}}
+
+{{--            .position-ref {--}}
+{{--                position: relative;--}}
+{{--            }--}}
+
+{{--            .top-right {--}}
+{{--                position: absolute;--}}
+{{--                right: 10px;--}}
+{{--                top: 18px;--}}
+{{--            }--}}
+
+{{--            .content {--}}
+{{--                text-align: center;--}}
+{{--            }--}}
+
+{{--            .title {--}}
+{{--                font-size: 84px;--}}
+{{--            }--}}
+
+{{--            .links > a {--}}
+{{--                color: #636b6f;--}}
+{{--                padding: 0 25px;--}}
+{{--                font-size: 13px;--}}
+{{--                font-weight: 600;--}}
+{{--                letter-spacing: .1rem;--}}
+{{--                text-decoration: none;--}}
+{{--                text-transform: uppercase;--}}
+{{--            }--}}
+
+{{--            .m-b-md {--}}
+{{--                margin-bottom: 30px;--}}
+{{--            }--}}
+{{--        </style>--}}
+{{--    </head>--}}
+{{--    <body>--}}
+{{--        <div class="flex-center position-ref full-height">--}}
+{{--            @if (Route::has('login'))--}}
+{{--                <div class="top-right links">--}}
+{{--                    @auth--}}
+{{--                        <a href="{{ url('/home') }}">Home</a>--}}
+{{--                    @else--}}
+{{--                        <a href="{{ route('login') }}">Login</a>--}}
+
+{{--                        @if (Route::has('register'))--}}
+{{--                            <a href="{{ route('register') }}">Register</a>--}}
+{{--                        @endif--}}
+{{--                    @endauth--}}
+{{--                </div>--}}
+{{--            @endif--}}
+
+{{--            <div class="content">--}}
+{{--                <div class="title m-b-md">--}}
+{{--                    Laravel--}}
+{{--                </div>--}}
+
+{{--                <div class="links">--}}
+{{--                    <a href="https://laravel.com/docs">Docs</a>--}}
+{{--                    <a href="https://laracasts.com">Laracasts</a>--}}
+{{--                    <a href="https://laravel-news.com">News</a>--}}
+{{--                    <a href="https://blog.laravel.com">Blog</a>--}}
+{{--                    <a href="https://nova.laravel.com">Nova</a>--}}
+{{--                    <a href="https://forge.laravel.com">Forge</a>--}}
+{{--                    <a href="https://vapor.laravel.com">Vapor</a>--}}
+{{--                    <a href="https://github.com/laravel/laravel">GitHub</a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </body>--}}
+{{--</html>--}}
